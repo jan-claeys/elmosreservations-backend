f1404ed594134ddb063823ba0f46b5b0f62d4477
jan.claeys <janclaeys01@gmail.com>
2022-04-13 13:01:08 +0200

    add validation for new reservation

--- src/reservations/dto/create-reservation.dto.ts
+++ src/reservations/dto/create-reservation.dto.ts
@@ -1,6 +1,24 @@
+import { Type } from 'class-transformer';
+import {IsDate, IsNotEmpty, IsNumber, MinDate } from 'class-validator';
+
 export class CreateReservationDto {
+    @IsNotEmpty()
+    @IsNumber()
     officeId: number;
+
+    @IsNotEmpty()
+    @IsNumber()
     userId: number;
+
+    @IsNotEmpty()
+    @Type(() => Date)
+    @IsDate()
+    @MinDate(new Date())
     start_date: Date;
+
+    @IsNotEmpty()
+    @Type(() => Date)
+    @IsDate()
+    @MinDate(new Date())
     end_date: Date;
 }
--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -15,7 +15,6 @@ export class ReservationsService {
   ) { }
 
   create(createReservationDto: CreateReservationDto) {
-    console.log(createReservationDto);
 
     this.reservationRepository.createQueryBuilder()
       .insert()
